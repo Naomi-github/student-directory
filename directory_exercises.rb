61e7df911d4457d138b3f9a8b78539826825eb2d
Naomi Starr <naomi.starr.mail@gmail.com>
2022-11-03 16:25:09 +0000

    string.center()

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,3 +1,5 @@
+#Research how the method center() of the String class works. Use it in your code to make the output beautifully aligned.
+
 def input_students
   puts 'Please enter the names of the students'
   puts 'To finish, just hit return twice'
@@ -18,8 +20,7 @@ def input_students
     age = gets.chomp
     # add the student to the array
     students << { name: name, cohort: :november, hobby: hobby, country: country, age: age }
-    puts "Now we have #{students.count} students"
-    puts "Students #{students}"
+    puts "Now we have #{students.count} students".center(100)
     # get another name from the user
     name = gets.chomp
   end
@@ -28,18 +29,18 @@ def input_students
 end
 
 def print_header
-  puts 'The students of Villains Academy'
-  puts '-------------'
+  puts 'The students of Villains Academy'.center(100)
+  puts '-------------'.center(100)
 end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort) (hobby:#{student[:hobby]} )(country of birth:#{student[:country]}) (age:#{student[:age]})"
+    puts "#{student[:name]} (#{student[:cohort]} cohort) (hobby:#{student[:hobby]} )(country of birth:#{student[:country]}) (age:#{student[:age]})".center(100)
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(100)
 end
 
 # nothing happens until we call the methods
